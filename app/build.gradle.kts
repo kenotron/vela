bcbdcabc28528262f2d091190ad5bec6b4ea92de
kenotron <kenotron@github.com>
2026-04-29 14:47:10 -0700

    fix(androidTest): add missing uiautomator dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -208,6 +208,7 @@
         androidTestImplementation(libs.androidx.test.runner)
         androidTestImplementation(libs.androidx.test.rules)
         androidTestImplementation(libs.androidx.espresso.core)
+        androidTestImplementation(libs.androidx.uiautomator)
         androidTestImplementation(libs.truth)
         androidTestImplementation(platform(libs.androidx.compose.bom))
         androidTestImplementation(libs.androidx.compose.ui.test.junit4)
